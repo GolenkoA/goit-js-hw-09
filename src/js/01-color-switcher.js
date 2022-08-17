53cdc9bf848b3d28ddaa882e11d61b56132b7bb9
Golenko Andrii <golenko.andrey@gmail.com>
2022-08-17 14:37:15 +0300

    task1
    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+const startBtnBackgroundColor = document.querySelector('button[data-start]');
+const stopBtnBackgroundColor = document.querySelector('button[data-stop]');
+
+stopBtnBackgroundColor.setAttribute('disabled', 'disabled');
+let IntervalId = '';
+
+startBtnBackgroundColor.addEventListener('click', startBackgroundColor);
+stopBtnBackgroundColor.addEventListener('click', stoptBackgroundColor);
+
+const TIME_OUT = 1000;
+
+function startBackgroundColor() {
+    
+    startBtnBackgroundColor.setAttribute('disabled', 'disabled');
+    stopBtnBackgroundColor.removeAttribute('disabled');
+    IntervalId = setInterval(() => document.body.style.backgroundColor = getRandomHexColor(), TIME_OUT);
+};
+
+function stoptBackgroundColor() {
+
+    clearInterval(IntervalId)
+    startBtnBackgroundColor.removeAttribute('disabled');
+    stopBtnBackgroundColor.setAttribute('disabled', 'disabled');
+};
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
